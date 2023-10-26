5c9722e915377968ed5614356748b185216d3879
Dion Gee <dion@wntr.com.au>
2023-10-26 11:58:14 +1000

    Removed the depend from slug page as it was flipping out in the console. Hero block hight was wrong.

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -13,7 +13,7 @@ export default function Index({ website, page }: { website: IWebsite, page: IPag
     useEffect(() => {
         context.setWebsite(website)
         context.setPage(page)
-    }, [website, page, context])
+    }, [website, page])
 
     return(
         <>
