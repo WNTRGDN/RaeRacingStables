f84ebd1b416bc47c75c97088ef556e2120378055
Dion Gee <dion@wntr.com.au>
2024-11-13 10:35:05 -0700

    fixed the webmanifest

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -24,7 +24,7 @@ export default function Index({ website, page }: { website: IWebsite, page: IPag
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest"></link>
+                <link rel="manifest" href="/site.webmanifest" />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content={(page.metaData.title ?? page.name) + ' | ' + website.name} />
                 <meta property="og:description" content={page.metaData.description} />
