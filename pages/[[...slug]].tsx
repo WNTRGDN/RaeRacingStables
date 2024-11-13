a00f3b07178d1ab1c8db3a40803a574addc0d5eb
Dion Gee <dion@wntr.com.au>
2024-11-13 10:33:59 -0700

    Added in Favicon

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -21,6 +21,10 @@ export default function Index({ website, page }: { website: IWebsite, page: IPag
                 <meta name="description" content={page.metaData.description} />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest"></link>
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content={(page.metaData.title ?? page.name) + ' | ' + website.name} />
                 <meta property="og:description" content={page.metaData.description} />
